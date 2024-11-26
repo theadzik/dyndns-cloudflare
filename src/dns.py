6924dbeb13508a4989b317d0166560a373facf81
Adam Żmuda <adzik007@gmail.com>
2024-11-26 03:54:10 +0100

    cloudflare api init

--- src/dns.py
+++ src/dns.py
@@ -9,12 +9,14 @@ import requests
 
 class HandlerDNS:
     BASE_URL = "https://api.cloudflare.com/client/v4"
-    APP_VERSION = os.environ['APP_VERSION']
-    HOSTNAME = os.environ["HOSTNAME"]
-    API_TOKEN = os.environ["API_TOKEN"]
 
     def __init__(self):
-        pass
+        self.APP_VERSION = os.environ['APP_VERSION']
+        self.HOSTNAME = os.environ["HOSTNAME"]
+        self.API_TOKEN = os.environ["API_TOKEN"]
+
+        self.zone_id = self.get_zone_id()
+        self.dns_record_id = self.get_dns_record_id()
 
     def get_headers(self) -> dict:
         headers = {
@@ -27,29 +29,39 @@ class HandlerDNS:
         }
         return headers
 
-    def update_dns_entry(self, zone_id: str, dns_record_id: str, ip_address: str) -> bool:
-        update_url = f"{self.BASE_URL}/zones/{zone_id}/dns_records/{dns_record_id}"
+    def get_zone_id(self) -> str:
+        url = f"{self.BASE_URL}/v4/zones"
+        response = requests.get(url, headers=self.get_headers())
+        zone_id = [zone["id"] for zone in response.json()["result"] if zone.name == self.HOSTNAME][0]
+        return zone_id
+
+    def get_dns_record_id(self) -> str:
+        url = f"{self.BASE_URL}/v4/zones/{self.zone_id}/dns_records"
+        response = requests.get(url, headers=self.get_headers())
+        record_id = [record["id"] for record in response.json()["result"] if record.name == self.HOSTNAME][0]
+        return record_id
+
+    def update_dns_entry(self, ip_address: str) -> bool:
+        update_url = f"{self.BASE_URL}/zones/{self.zone_id}/dns_records/{self.dns_record_id}"
 
         data = {
             "name": f"{self.HOSTNAME}",
-            "proxied": True,
             "ttl": 60,
             "content": f"{ip_address}",
             "type": "A"
         }
 
-        update = requests.put(
+        response = requests.patch(
             url=update_url,
             data=data,
             headers=self.get_headers(),
         )
 
-        result = self.handle_response(update)
+        result = self.handle_response(response)
         return result
 
     @staticmethod
     def handle_response(response: requests.Response) -> bool:
-        # https://www.noip.com/integrate/response
         if re.match(r"good.*", response.text):
             logging.info(f"Updated DNS. [{response.status_code}] {response.text}")
             return True
